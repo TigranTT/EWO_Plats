d17080a1dee0774c966922a23083a83af70acaa9
TigranTT <tigrantt@yahoo.com>
2018-01-28 22:23:23 -0600

    added more info in to PIN array

--- EWO Plats/EWO Plats/Model/PinInfoArray.swift
+++ EWO Plats/EWO Plats/Model/PinInfoArray.swift
@@ -6,11 +6,21 @@
 //  Copyright © 2018 Tigran Tshorokhyan. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-var pinInfo =  [Pin(title: "60519 N Ridgewood Dr Slidell, LA  70460", pinURL: "https://att.box.com/s/9hn9b1wno6korq3bc6i474s3ka6jnw7r"),
+let pinInfo =  [Pin(title: "60519 N Ridgewood Dr Slidell, LA  70460", pinURL: "https://att.box.com/s/9hn9b1wno6korq3bc6i474s3ka6jnw7r"),
                 Pin(title: "41041 Crawford Landing Rd Slidell, LA  70461", pinURL: "https://att.box.com/s/z1o7s6xk976pnoo778eh5grldcfafyve"),
-                Pin(title: "39419 Dean Rd Pearl River, LA  70458", pinURL: "https://att.box.com/s/znq7j8wcbal8bm19364ssn9djwta7nwo")]
+                Pin(title: "39419 Dean Rd Pearl River, LA  70458", pinURL: "https://att.box.com/s/znq7j8wcbal8bm19364ssn9djwta7nwo"),
+                Pin(title: "3596 Lakeshore Blvd E Slidell, LA  70461", pinURL: "https://att.box.com/s/oqmx16b72l4skm20qbh69r2dqmarvrli"),
+                Pin(title: "1336 Lindberg Dr Slidell, LA  70458", pinURL: "https://att.box.com/s/8m5v8l3lap9199zesqsln4nysausurli"),
+                Pin(title: "1006 Breckenridge Dr Slidell, LA  70461", pinURL: "https://att.box.com/s/0deog0zi0bvn7ye5t50tkdv02a94y7eh"),
+                Pin(title: "1001 Mariners Cove Blvd Slidell, LA  70458", pinURL: "https://att.box.com/s/8ccq6rms3hocrwd9cbws8ndtbr0s18cs"),
+                Pin(title: "350 Spartan Dr Slidell, LA  70458", pinURL: "https://att.box.com/s/7ri8rzceiwpmp8klz7u6ve5sxlgu8oy5"),
+                Pin(title: "134 Kingspoint Blvd Slidell, LA  70461", pinURL: "https://att.box.com/s/dvxwy630c7m0ur985ifxgs4nqw23rj38"),
+                Pin(title: "130 Herwig Bluff Rd Slidell, LA  70461", pinURL: "https://att.box.com/s/mu2gxafahoouxky1hp8xhvqcv9yn4ehx"),
+                Pin(title: "108 Cross Creek Dr Slidell, LA  70461", pinURL: "https://att.box.com/s/w9frc05qkh9qxfudba5yh2buzb01f3z6"),
+                Pin(title: "101 Lakeshore Vlg S Slidell, LA  70461", pinURL: "https://att.box.com/s/3qyp3x6csrwwi6rjgx2rkjyi89c7o0xx"),
+                Pin(title: "100 Robert Blvd Slidell, LA  70458", pinURL: "https://att.box.com/s/cv0rlamf21mnmklclzyks8qciu2hxrwm"),]
 
 
 
